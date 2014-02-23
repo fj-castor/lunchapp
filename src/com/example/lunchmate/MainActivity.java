96a6ce69291b7931aeecd0fab335fec8128f38f6
NichPham <nichpham@hotmail.com>
2014-02-23 06:00:16 -0500

    Created main activity and layout, no background

--- src/com/example/lunchmate/MainActivity.java
+++ src/com/example/lunchmate/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.lunchmate;
 
-import android.os.Bundle;
+import android.app.ActionBar;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
@@ -10,15 +10,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		ActionBar actionBar = getActionBar();
+		actionBar.hide();
 	}
-	
-	
-
-//	@Override
-//	public boolean onCreateOptionsMenu(Menu menu) {
-//		// Inflate the menu; this adds items to the action bar if it is present.
-//		getMenuInflater().inflate(R.menu.activity_main, menu);
-//		return true;
-//	}
-
 }
