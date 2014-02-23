4e0e776dafc3c1b479359bae32bd605436345470
NichPham <nichpham@hotmail.com>
2014-02-23 07:42:21 -0500

    Added waiting for connection page.

---
+++ src/com/example/lunchmate/ConnectionActivity.java
@@ -0,0 +1,23 @@
+package com.example.lunchmate;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class ConnectionActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		setContentView(R.layout.activity_connection);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.connection, menu);
+		return true;
+	}
+
+}
--- src/com/example/lunchmate/MainActivity.java
+++ src/com/example/lunchmate/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.lunchmate;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -13,5 +15,10 @@ public class MainActivity extends Activity {
 		
 		ActionBar actionBar = getActionBar();
 		actionBar.hide();
+	}
+	
+	public void button(View view) {
+		Intent intent = new Intent(this, ConnectionActivity.class);
+		startActivity(intent);
 	}
 }
